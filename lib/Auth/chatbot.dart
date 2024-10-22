ced0744981a9018bb5f609f5f07c68ad9c8872c2
Shaden Al-bader <shadnalbadr@gmail.com>
2024-10-22 03:17:32 +0300

    fix delete in profile

---
+++ lib/Auth/chatbot.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class chatbot extends StatefulWidget {
+  const chatbot({super.key});
+
+  @override
+  State<chatbot> createState() => _chatbotState();
+}
+
+class _chatbotState extends State<chatbot> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
