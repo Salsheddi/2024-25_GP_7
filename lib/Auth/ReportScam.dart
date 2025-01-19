404275bb445705d12aea8660bdff9ea12a3ffdc5
Shaden Al-bader <shadnalbadr@gmail.com>
2025-01-19 06:39:47 +0300

    image to text in reportscam

--- lib/Auth/ReportScam.dart
+++ lib/Auth/ReportScam.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 
 class ReportScam extends StatefulWidget {
   const ReportScam({Key? key}) : super(key: key);
@@ -18,6 +21,68 @@ class _ReportScamState extends State<ReportScam>
   final TextEditingController _messageController = TextEditingController();
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
   final FirebaseAuth auth = FirebaseAuth.instance;
+
+// convert image to text
+  String? _result; // To store the result from the API
+  File? _image; // To store image
+  final ImagePicker _picker =
+      ImagePicker(); // Create an instance of ImagePicker
+
+  String? _extractedText = ''; // Store the initial extracted text
+  bool _isTextEdited = false; // Flag to track if the text has been edited
+  bool _isTextSet = false; // Flag to track if the extracted text has been set
+
+  Future<void> _pickImage() async {
+    final XFile? pickedFile =
+        await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path); // Store the image file
+        _messageController
+            .clear(); // Clear any existing text in the field immediately
+        _isTextEdited =
+            false; // Reset the edit flag to prevent premature image removal
+        _extractedText = ''; // Reset the extracted text
+        _isTextSet = false; // Reset the flag to false
+      });
+
+      // Extract text from the image once uploaded
+      await _extractTextFromImage();
+    }
+  }
+
+  Future<void> _extractTextFromImage() async {
+    if (_image == null) return;
+    try {
+      final textRecognizer = TextRecognizer();
+      final InputImage inputImage = InputImage.fromFile(_image!);
+      final RecognizedText recognizedText =
+          await textRecognizer.processImage(inputImage);
+
+      setState(() {
+        // Set extracted text to the text field
+        _messageController.text = recognizedText.text;
+        _extractedText = recognizedText.text; // Store the extracted text
+        _isTextSet = true; // Mark that the extracted text has been set
+      });
+
+      textRecognizer.close();
+    } catch (e) {
+      setState(() {
+        _result = "Error extracting text: $e";
+      });
+    }
+  }
+
+// Reset function
+  void _resetContent() {
+    _messageController.clear(); // Clear the text field
+    setState(() {
+      _image = null; // Clear the image
+      _result = null; // Clear the result
+      _isTextSet = false; // Reset the text set flag
+    });
+  }
 
   @override
   void initState() {
@@ -37,71 +102,72 @@ class _ReportScamState extends State<ReportScam>
   }
 
   Future<void> reportMessage(String content) async {
-  final String userId = auth.currentUser?.uid ?? ''; // Get the current user's ID
-  if (userId.isEmpty) {
-    _showMessage('User not logged in.');
-    return;
-  }
-
-  final String contentHash = generateContentHash(content);
-
-  // Check if the user has already reported this message
-  final QuerySnapshot existingReport = await firestore
-      .collection('reportedMessages')
-      .where('contentHash', isEqualTo: contentHash)
-      .where('userId', isEqualTo: userId)
-      .get();
-
-  if (existingReport.docs.isNotEmpty) {
-    _showAlreadyReportedDialog();
-    return;
-  }
-
-  final String messageId = "RE${DateTime.now().millisecondsSinceEpoch}";
-
-  // Add the new report
-  await firestore.collection('reportedMessages').doc(messageId).set({
-    'userId': userId,
-    'content': content,
-    'reportedAt': FieldValue.serverTimestamp(),
-    'contentHash': contentHash,
-  });
-
-  // Increment the report count
-  await incrementReportCount(contentHash, content);
-
-  _showMessage('Message reported successfully.');
-}
-
-Future<void> incrementReportCount(String contentHash, String content) async {
-  final DocumentReference summaryDoc =
-      firestore.collection('reportedMessagesSummary').doc(contentHash);
-
-  await firestore.runTransaction((transaction) async {
-    final snapshot = await transaction.get(summaryDoc);
-
-    if (!snapshot.exists) {
-      // Initialize the count if it doesn't exist
-      transaction.set(summaryDoc, {
-        'content': content,
-        'reportCount': 1,
-        'reportedUsers': [auth.currentUser?.uid],
-      });
-    } else {
-      final data = snapshot.data() as Map<String, dynamic>;
-      final List<dynamic> reportedUsers = data['reportedUsers'] ?? [];
-
-      if (!reportedUsers.contains(auth.currentUser?.uid)) {
-        // Add the user to the reported users list if not already reported
-        reportedUsers.add(auth.currentUser?.uid);
-        transaction.update(summaryDoc, {
-          'reportCount': reportedUsers.length,
-          'reportedUsers': reportedUsers,
-        });
-      }
+    final String userId =
+        auth.currentUser?.uid ?? ''; // Get the current user's ID
+    if (userId.isEmpty) {
+      _showMessage('User not logged in.');
+      return;
     }
-  });
-}
+
+    final String contentHash = generateContentHash(content);
+
+    // Check if the user has already reported this message
+    final QuerySnapshot existingReport = await firestore
+        .collection('reportedMessages')
+        .where('contentHash', isEqualTo: contentHash)
+        .where('userId', isEqualTo: userId)
+        .get();
+
+    if (existingReport.docs.isNotEmpty) {
+      _showAlreadyReportedDialog();
+      return;
+    }
+
+    final String messageId = "RE${DateTime.now().millisecondsSinceEpoch}";
+
+    // Add the new report
+    await firestore.collection('reportedMessages').doc(messageId).set({
+      'userId': userId,
+      'content': content,
+      'reportedAt': FieldValue.serverTimestamp(),
+      'contentHash': contentHash,
+    });
+
+    // Increment the report count
+    await incrementReportCount(contentHash, content);
+
+    _showMessage('Message reported successfully.');
+  }
+
+  Future<void> incrementReportCount(String contentHash, String content) async {
+    final DocumentReference summaryDoc =
+        firestore.collection('reportedMessagesSummary').doc(contentHash);
+
+    await firestore.runTransaction((transaction) async {
+      final snapshot = await transaction.get(summaryDoc);
+
+      if (!snapshot.exists) {
+        // Initialize the count if it doesn't exist
+        transaction.set(summaryDoc, {
+          'content': content,
+          'reportCount': 1,
+          'reportedUsers': [auth.currentUser?.uid],
+        });
+      } else {
+        final data = snapshot.data() as Map<String, dynamic>;
+        final List<dynamic> reportedUsers = data['reportedUsers'] ?? [];
+
+        if (!reportedUsers.contains(auth.currentUser?.uid)) {
+          // Add the user to the reported users list if not already reported
+          reportedUsers.add(auth.currentUser?.uid);
+          transaction.update(summaryDoc, {
+            'reportCount': reportedUsers.length,
+            'reportedUsers': reportedUsers,
+          });
+        }
+      }
+    });
+  }
 
   void _showAlreadyReportedDialog() {
     showDialog(
@@ -128,7 +194,8 @@ Future<void> incrementReportCount(String contentHash, String content) async {
   }
 
   void _showMessage(String message) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
 
   // Fetch all the reported messages from the Community tab
@@ -147,6 +214,11 @@ Future<void> incrementReportCount(String contentHash, String content) async {
 
   @override
   Widget build(BuildContext context) {
+    // Notifiers for hover effects
+    ValueNotifier<bool> isImageHovered = ValueNotifier(false);
+    ValueNotifier<bool> isButtonHovered = ValueNotifier(false);
+    ValueNotifier<bool> isButtonPressed = ValueNotifier(false);
+
     return Scaffold(
       backgroundColor: const Color(0xFFEDECEC),
       body: Stack(
@@ -225,9 +297,17 @@ Future<void> incrementReportCount(String contentHash, String content) async {
                         // Report Tab
                         Padding(
                           padding: const EdgeInsets.only(
-                              top: 45.0, left: 30, right: 30),
+                              top: 25.0, left: 30, right: 30),
                           child: Column(
                             children: [
+                              const Text(
+                                "Help us keep the community safe by reporting any fraudulent messages.",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              const SizedBox(height: 20),
                               // Text area with delete button
                               Stack(
                                 clipBehavior: Clip.none,
@@ -239,7 +319,8 @@ Future<void> incrementReportCount(String contentHash, String content) async {
                                       hintText: 'Enter text here..',
                                       hintStyle: const TextStyle(
                                         fontSize: 15,
-                                        color: Color.fromARGB(255, 137, 136, 136),
+                                        color:
+                                            Color.fromARGB(255, 137, 136, 136),
                                       ),
                                       border: OutlineInputBorder(
                                           borderRadius:
@@ -249,10 +330,12 @@ Future<void> incrementReportCount(String contentHash, String content) async {
                                       suffixIcon: IconButton(
                                         onPressed: () {
                                           // Handle file upload
+                                          _pickImage();
                                         },
                                         icon: Icon(Icons.attach_file,
                                             size: 25,
-                                            color: Colors.blue.withOpacity(0.65)),
+                                            color:
+                                                Colors.blue.withOpacity(0.65)),
                                       ),
                                     ),
                                   ),
@@ -261,7 +344,8 @@ Future<void> incrementReportCount(String contentHash, String content) async {
                                     left: -10,
                                     child: GestureDetector(
                                       onTap: () {
-                                        _messageController.clear(); // Clear text
+                                        _messageController
+                                            .clear(); // Clear text
                                       },
                                       child: Container(
                                         width: 24,
@@ -280,13 +364,68 @@ Future<void> incrementReportCount(String contentHash, String content) async {
                                   ),
                                 ],
                               ),
+                              const SizedBox(height: 20),
 
+                              if (_image != null)
+                                Stack(
+                                  clipBehavior: Clip.none,
+                                  children: [
+                                    ValueListenableBuilder<bool>(
+                                      valueListenable: isImageHovered,
+                                      builder: (context, hover, child) {
+                                        return MouseRegion(
+                                          onEnter: (_) =>
+                                              isImageHovered.value = true,
+                                          onExit: (_) =>
+                                              isImageHovered.value = false,
+                                          child: AnimatedContainer(
+                                            duration: const Duration(
+                                                milliseconds: 200),
+                                            height: hover ? 140 : 120,
+                                            width: hover ? 140 : 120,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(8.0),
+                                              border: Border.all(
+                                                  color: Colors.grey, width: 1),
+                                            ),
+                                            child: Image.file(
+                                              _image!,
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                    Positioned(
+                                      top: -10,
+                                      left: -10,
+                                      child: GestureDetector(
+                                        onTap: _resetContent,
+                                        child: Container(
+                                          width: 24,
+                                          height: 24,
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Colors.red,
+                                          ),
+                                          child: const Icon(
+                                            Icons.close,
+                                            color: Colors.white,
+                                            size: 16,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               const SizedBox(height: 20),
 
                               // Report Message button
                               ElevatedButton(
                                 onPressed: () {
-                                  final content = _messageController.text.trim();
+                                  final content =
+                                      _messageController.text.trim();
                                   if (content.isEmpty) {
                                     _showMessage('Please enter a message.');
                                     return;
@@ -304,7 +443,8 @@ Future<void> incrementReportCount(String contentHash, String content) async {
                                 ),
                                 child: const Text(
                                   "Report Message",
-                                  style: TextStyle(fontSize: 16, color: Colors.white),
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.white),
                                 ),
                               ),
                             ],
@@ -315,12 +455,15 @@ Future<void> incrementReportCount(String contentHash, String content) async {
                         StreamBuilder<List<Map<String, dynamic>>>(
                           stream: getReportedMessages(),
                           builder: (context, snapshot) {
-                            if (snapshot.connectionState == ConnectionState.waiting) {
-                              return const Center(child: CircularProgressIndicator());
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const Center(
+                                  child: CircularProgressIndicator());
                             }
 
                             if (snapshot.hasError) {
-                              return const Center(child: Text('Error loading data.'));
+                              return const Center(
+                                  child: Text('Error loading data.'));
                             }
 
                             final reportedMessages = snapshot.data ?? [];
@@ -331,7 +474,8 @@ Future<void> incrementReportCount(String contentHash, String content) async {
                                 final message = reportedMessages[index];
                                 return ListTile(
                                   title: Text(message['content']),
-                                  subtitle: Text('Reported ${message['reportCount']} times'),
+                                  subtitle: Text(
+                                      'Reported ${message['reportCount']} times'),
                                 );
                               },
                             );
@@ -371,4 +515,3 @@ Future<void> incrementReportCount(String contentHash, String content) async {
     );
   }
 }
-
